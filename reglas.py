6fd7ad7fda160de32de35a8e8dc4c95972fdb4f7
Santicm23 <santicastrom@outlook.com>
2023-09-30 16:40:04 -0500

    terminando funcion obtener lado

--- reglas.py
+++ reglas.py
@@ -1,10 +1,40 @@
 
+from typing import Literal
+
 # "∀x Romano(x) ⇒ (Leal(x, Cesar) ⇔ Odia(x, Cesar))"
+
+
+l = ['∀', '∃', '¬', '∧', '∨', '⇒', '⇔', '(', ')', ',', ' ']
+
+
+def obtener_lado(cadena: str, char: str, lado: Literal['i', 'd']) -> str:
+    step = 1 if lado == 'd' else -1
+    p1 = ')' if lado == 'd' else '('
+    p2 = '(' if lado == 'd' else ')'
+
+    index = cadena.find(char)
+
+    i = index
+
+    parentesis = 0
+
+    while (char != p1 or parentesis != -1) and i < len(cadena) - 1 and i > 0:
+        i += step
+        char = cadena[i]
+
+        if char == p2:
+            parentesis += 1
+        elif char == p1:
+            parentesis -= 1
+
+    res = cadena[index+1:i+1] if lado == 'd' else cadena[i:index]
+
+    return res
 
 
 def eliminar_bicondicional(cadena: str) -> str:
     """Elimina el bicondicional de la cadena de entrada."""
-    
+
     index = cadena.find("⇔")
 
     i = index
@@ -20,8 +50,8 @@ def eliminar_bicondicional(cadena: str) -> str:
             closed_parentesis += 1
         elif char == '(':
             closed_parentesis -= 1
-    
+
     return cadena[i+1:index]
 
-print(eliminar_bicondicional("∀x Romano(x) ⇒ (Leal(x, Cesar) ⇔ Odia(x, Cesar))"))
-    
+
+print(obtener_lado("∀x Romano(x) ⇒ (Leal(x, Cesar) ⇔ Odia(x, Cesar))", '⇔', 'd'))
