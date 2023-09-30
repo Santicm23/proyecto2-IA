d5b80368043fd45db41650d122f0212dc9a89043
Santicm23 <santicastrom@outlook.com>
2023-09-30 16:05:16 -0500

    primer elemento bicondicional

--- reglas.py
+++ reglas.py
@@ -7,21 +7,21 @@ def eliminar_bicondicional(cadena: str) -> str:
     
     index = cadena.find("⇔")
 
-    i = index - 1
+    i = index
 
     char = ''
     closed_parentesis = 0
 
-    while char != '(' or closed_parentesis != 0:
-        char = cadena[i]
+    while char != '(' or closed_parentesis != -1:
         i -= 1
+        char = cadena[i]
 
         if char == ')':
             closed_parentesis += 1
         elif char == '(':
             closed_parentesis -= 1
     
-    return cadena[i:index + 1]
+    return cadena[i+1:index]
 
 print(eliminar_bicondicional("∀x Romano(x) ⇒ (Leal(x, Cesar) ⇔ Odia(x, Cesar))"))
     
