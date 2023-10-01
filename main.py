67a7da5679d038b0dca82138fc37b97c8d579503
Santicm23 <santicastrom@outlook.com>
2023-10-01 17:57:43 -0500

    tests

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 
-'''
-1. ¬Niño(Scrooge) ∨ Ama(Scrooge,PapáNoel)
-2. ¬Ama(Scrooge,PapáNoel) ∨ ¬Reno(Rodolfo) ∨ Ama(Scrooge,Rodolfo)
-3. Reno(Rodolfo)
-4. NarizRoja(Rodolfo)
-5. ¬NarizRoja(Rodolfo) ∨ Raro(Rodolfo) ∨ Payaso(Rodolfo)
-6. ¬Reno(Rodolfo) ∨ ¬Payaso(Rodolfo)
-7. ¬Raro(Rodolfo) ∨ ¬Ama(Scrooge,Rodolfo)
-
-¬Niño(Scrooge)?
-'''
-
 from helpers.resolucion import inferencia_resolucion
+from tests.ejemplos_clase import ejemplo1, ejemplo2
 
 
 def main() -> None:
-    clausulas = [
-        '¬Niño(Scrooge) ∨ Ama(Scrooge,PapáNoel)',
-        '¬Ama(Scrooge,PapáNoel) ∨ ¬Reno(Rodolfo) ∨ Ama(Scrooge,Rodolfo)',
-        'Reno(Rodolfo)',
-        'NarizRoja(Rodolfo)',
-        '¬NarizRoja(Rodolfo) ∨ Raro(Rodolfo) ∨ Payaso(Rodolfo)',
-        '¬Reno(Rodolfo) ∨ ¬Payaso(Rodolfo)',
-        '¬Raro(Rodolfo) ∨ ¬Ama(Scrooge,Rodolfo)'
-    ]
 
-    pregunta = '¬Niño(Scrooge)'
-
-    print(inferencia_resolucion(clausulas, pregunta))
+    print(inferencia_resolucion(ejemplo2['clausulas'], ejemplo2['pregunta']))
 
 
 if __name__ == '__main__':
---
+++ tests/ejemplos_clase.py
@@ -0,0 +1,49 @@
+'''
+1. ¬Niño(Scrooge) ∨ Ama(Scrooge,PapáNoel)
+2. ¬Ama(Scrooge,PapáNoel) ∨ ¬Reno(Rodolfo) ∨ Ama(Scrooge,Rodolfo)
+3. Reno(Rodolfo)
+4. NarizRoja(Rodolfo)
+5. ¬NarizRoja(Rodolfo) ∨ Raro(Rodolfo) ∨ Payaso(Rodolfo)
+6. ¬Reno(Rodolfo) ∨ ¬Payaso(Rodolfo)
+7. ¬Raro(Rodolfo) ∨ ¬Ama(Scrooge,Rodolfo)
+
+¬Niño(Scrooge)?
+'''
+
+ejemplo1 = {
+    'clausulas': [
+        '¬Niño(Scrooge) ∨ Ama(Scrooge,PapáNoel)',
+        '¬Ama(Scrooge,PapáNoel) ∨ ¬Reno(Rodolfo) ∨ Ama(Scrooge,Rodolfo)',
+        'Reno(Rodolfo)',
+        'NarizRoja(Rodolfo)',
+        '¬NarizRoja(Rodolfo) ∨ Raro(Rodolfo) ∨ Payaso(Rodolfo)',
+        '¬Reno(Rodolfo) ∨ ¬Payaso(Rodolfo)',
+        '¬Raro(Rodolfo) ∨ ¬Ama(Scrooge,Rodolfo)'
+    ],
+    'pregunta': '¬Niño(Scrooge)'
+}
+
+'''
+1. Hombre(Marco)
+2. Pompeyano(Marco)
+3. ¬Pompeyano(Marco) ∨ Romano(Marco)
+4. Gobernante(Cesar)
+5. ¬Romano(Marco) ∨ Leal(Marco,Cesar) ∨ Odia(Marco,Cesar)
+6. ¬Hombre(Marco) ∨ ¬Gobernante(Cesar) ∨ ¬IntentaAsesinar(Marco,Cesar) ∨ ¬Leal(Marco,Cesar)
+7. IntentaAsesinar(Marco,Cesar)
+
+Odia(Marco,Cesar)?
+'''
+
+ejemplo2 = {
+    'clausulas': [
+        'Hombre(Marco)',
+        'Pompeyano(Marco)',
+        '¬Pompeyano(Marco) ∨ Romano(Marco)',
+        'Gobernante(Cesar)',
+        '¬Romano(Marco) ∨ Leal(Marco,Cesar) ∨ Odia(Marco,Cesar)',
+        '¬Hombre(Marco) ∨ ¬Gobernante(Cesar) ∨ ¬IntentaAsesinar(Marco,Cesar) ∨ ¬Leal(Marco,Cesar)',
+        'IntentaAsesinar(Marco,Cesar)'
+    ],
+    'pregunta': 'Odia(Marco,Cesar)'
+}
