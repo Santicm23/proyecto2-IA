d82f74c719b24324f571d354ecf80eeba8716816
Santicm23 <santicastrom@outlook.com>
2023-10-05 15:16:10 -0500

    evidencias

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tests.ejemplos_clase import ejemplo1, ejemplo2, ejemplo3, ejemplo4, ejemplo
 
 def main() -> None:
 
-    print(inferencia_resolucion(ejemplo3['clausulas'], ejemplo3['pregunta']))
+    print(inferencia_resolucion(ejemplo5['clausulas'], ejemplo5['pregunta']))
     
 
 
