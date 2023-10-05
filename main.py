4c5b343f39e19db7eecb67f01426606b0612aa84
Santicm23 <santicastrom@outlook.com>
2023-10-05 16:11:21 -0500

    leer el archivo

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 
+import sys
+
 from helpers.resolucion import inferencia_resolucion
 from tests.ejemplos_clase import ejemplo1, ejemplo2, ejemplo3, ejemplo4, ejemplo5
 from helpers.archivo import leer_clausulas
 
 
 def main() -> None:
+    """Función principal del programa."""
 
-    ejemplo = leer_clausulas('tests/ejemplo-archivo.txt')
+    ruta: str
+    try:
+        ruta = sys.argv.pop(1)
+    except IndexError:
+        print('No se ingreso la dirección del archivo.')
+        return
+
+    ejemplo = leer_clausulas(ruta)
 
     print(inferencia_resolucion(ejemplo['clausulas'], ejemplo['pregunta']))
 
